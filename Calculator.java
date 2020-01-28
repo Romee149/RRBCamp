401124a51c502d8c19a43c8b2de46261d2f95110
romee <romeeafroz@yahoo.com>
2020-01-27 22:48:38 -0600

    first commit

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int x=90;
+		int y=30;
+		int add=x+y;
+		int sub=x-y;
+		int mul=x*y;
+		int div=x/y;
+		System.out.println ("The Tolal is:" +add);
+		System.out.println("The Substaction is:"+ sub);
+		System.out.println("The Multiplication is:"+ mul);
+		System.out.println("The Division is:"+ div);
+
+		
+
+	}
+
+}
