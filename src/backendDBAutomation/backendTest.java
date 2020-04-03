4c572efd3133afc0c30bbeed81050cb9aa5b2a8b
romee <romeeafroz@yahoo.com>
2020-04-02 22:00:05 -0500

    new add

---
+++ src/backendDBAutomation/backendTest.java
@@ -0,0 +1,14 @@
+package backendDBAutomation;
+public class backendTest
+{
+
+public static void main (String[] args) {
+
+//public static int addtest() {
+int a= 67;
+int b= 44;
+ int add=a+b;
+System.out.println(add);
+//return a+b;
+
+}}
