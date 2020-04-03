b9b1e8b165698e4270ce183c0ada4dcc456d760c
romee <romeeafroz@yahoo.com>
2020-04-02 22:21:21 -0500

    edited

--- src/backendDBAutomation/gittest1.java
+++ src/backendDBAutomation/gittest1.java
@@ -3,6 +3,6 @@ package backendDBAutomation;
 public class gittest1 {
 public static void linklist() {
 	System.out.println("We will see the link Text");
-Sytem.out.println("add more topics");
+System.out.println("add more topics");
 }
 }
